6db2fd88084b191309dc3c23063ca5ffb1cb48c0
Prasanth123-c <prasanthc267@gamil.com>
2023-11-21 23:49:22 +0530

    first commit

---
+++ app1/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+# Create your views here.
+def sindu(request):
+    return render(request,'sindu.html')
---
+++ app2/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+# Create your views here.
+def sidu(request):
+    return render(request,'sidu.html')
